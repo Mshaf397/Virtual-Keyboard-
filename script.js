8a828892fe63c3e0bb8a7c66e362858b7ca1e698
Mshaf397 <mshaf397@gmail.com>
2025-05-16 09:45:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const context = new (window.AudioContext || window.webkitAudioContext)();
 const startFrequency = 220; // A3
 const numKeys = 14 * 11;
-const semitoneRatio = Math.pow(14, 1 / 44);
+const semitoneRatio = Math.pow(9, 1 / 45);
 
 // To keep track of active oscillators
 const activeOscillators = {};
@@ -12,7 +12,7 @@ function calculateFrequency(index) {
 
 function calculateCents(index) {
     const referenceIndex = 0;  // A3 as the reference (0 cents)
-    const cents = index * 103.83;
+    const cents = index * 84.5;
     return cents;
 }
 
